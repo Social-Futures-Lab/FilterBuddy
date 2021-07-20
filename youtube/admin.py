b62617cf4d3421b9030a0ae5f5f78dcb0e3a2eba
Server <unknown@wordfilters.railgun.in>
2021-07-20 19:53:58 +0000

    Edited admin

--- youtube/admin.py
+++ youtube/admin.py
@@ -4,11 +4,27 @@ from .models import Channel, RuleCollection, Rule, Video, Comment, Reply
 
 # Register your models here.
 
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date')
+
 class RuleCollectionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'channel')
+    list_display = ('title', 'channel', 'id')
 
 class RuleAdmin(admin.ModelAdmin):
-    list_display = ('phrase', 'rule_collection')
+    list_display = ('phrase', 'rule_collection', 'id')
 
-admin.site.register(Rule)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'channel', 'video_id')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'video', 'pub_date', 'author', 'likeCount', 'comment_id')
+
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author', 'likeCount', 'comment', 'reply_id')
+
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(Rule, RuleAdmin)
 admin.site.register(RuleCollection, RuleCollectionAdmin)
+admin.site.register(Video, VideoAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Reply, ReplyAdmin)
