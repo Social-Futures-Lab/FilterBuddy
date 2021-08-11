8d742fff97084e199040d4583568ea9cf19b1d44
Jim Chen <knh.jabbany@gmail.com>
2021-08-11 16:35:05 -0700

    Fix typo

--- youtube/views.py
+++ youtube/views.py
@@ -40,7 +40,7 @@ class YouTubeForm(forms.Form):
 def indicate_active(current, reference_page, reference_arg = None):
   return 'active' if current['page'] == reference_page and (
     (current['collection'] is None and reference_arg is None) or
-    (current['collection'] == reference_arg) else ''
+    (current['collection'] == reference_arg)) else ''
 
 class HomePageView(FormView):
   template_name = 'youtube/home.html'
