a0b35a6370782f49920ac2b7dfa6f64e2c2ba81d
Jim Chen <knh.jabbany@gmail.com>
2021-08-11 16:46:43 -0700

    Change tag name again

--- youtube/views.py
+++ youtube/views.py
@@ -36,7 +36,7 @@ class YouTubeForm(forms.Form):
 
 
 # Register the filters
-@register.simple_tag(name='sidebar')
+@register.simple_tag(name='sidebar-active')
 def indicate_active(current, reference_page, reference_arg = None):
   return 'active' if current['page'] == reference_page and (
     (current['collection'] is None and reference_arg is None) or
