9ca770edf124d1435aaefe5b6e371d535fd11ac8
Jim Chen <knh.jabbany@gmail.com>
2021-08-11 16:42:53 -0700

    Rename macro

--- youtube/views.py
+++ youtube/views.py
@@ -36,7 +36,7 @@ class YouTubeForm(forms.Form):
 
 
 # Register the filters
-@register.simple_tag(name='sidebar-indicate-active')
+@register.simple_tag(name='sidebar')
 def indicate_active(current, reference_page, reference_arg = None):
   return 'active' if current['page'] == reference_page and (
     (current['collection'] is None and reference_arg is None) or
