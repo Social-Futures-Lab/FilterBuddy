845c1c5fb55a2b21bf06a8326547e7dabc05d1bf
Jim Chen <knh.jabbany@gmail.com>
2022-05-11 17:00:22 -0700

    Remove unused code

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('', include(('youtube.urls', 'youtube'), namespace='youtube')),
 ]
